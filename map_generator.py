51052b899e147d99b0c2c6fd55718628f721cdc1
Christoph Weber <christoph.t.weber@gmail.com>
2019-03-27 22:48:34 +0100

    added randomizer for amount of items, now only dot-symmetry

--- map_generator.py
+++ map_generator.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from config import *
@@ -10,7 +11,7 @@ def generate_random_field(name, entropy, cell_type_counts):
 
     Usage:
     >>> default_counts = {
-            CELL_FENCE: 3,
+            CELL_FENCE: 10,
             CELL_RHUBARB: 1,
             CELL_GRASS: 8
         }
@@ -27,7 +28,22 @@ def generate_random_field(name, entropy, cell_type_counts):
         assert next_position in range(length)
         return next_position
 
-    def place_cells(field, cell_type, count, point_mirror=False, mirror_type=None):
+    def place_cells(field, cell_type, count, mirror_type=None):
+        mirror_type = mirror_type or cell_type
+        previous_x = random.randrange(0, round(FIELD_WIDTH))
+        previous_y = random.randrange(0, round(FIELD_HEIGHT / 2))
+        for _ in range(count):
+            for _ in range(int(10 / entropy)):
+                x = next_position(previous_x, round(FIELD_WIDTH), entropy)
+                y = next_position(previous_y, round(FIELD_HEIGHT / 2), entropy)
+                if field[y][x] == CELL_EMPTY:
+                    field[y][x] = cell_type
+
+                    field[-(y + 1)][-(x + 1)] = mirror_type
+                    previous_x, previous_y = x, y
+                    break
+
+    def place_cells2(field, cell_type, count, point_mirror=False, mirror_type=None):
         mirror_type = mirror_type or cell_type
         previous_x = random.randrange(0, round(FIELD_WIDTH / 2))
         previous_y = random.randrange(0, round(FIELD_HEIGHT / 2))
@@ -54,7 +70,7 @@ def generate_random_field(name, entropy, cell_type_counts):
         :param name: name of the file
         :param map: generated map
         """
-        with open(os.path.join('resources', f'{name}.map'), 'w') as file:
+        with open(os.path.join('resources', 'random_maps', f'{name}.map'), 'w') as file:
             for row in map:
                 for cell in row:
                     file.write(cell)
@@ -62,16 +78,38 @@ def generate_random_field(name, entropy, cell_type_counts):
 
     for cell_type, count in cell_type_counts.items():
         place_cells(field, cell_type, count)
+        # place_cells(field, cell_type, count)
 
-    place_cells(field, CELL_SHEEP_1, 1, point_mirror=True, mirror_type=CELL_SHEEP_2)
-    place_cells(field, CELL_WOLF_1, 1, point_mirror=True, mirror_type=CELL_WOLF_2)
+    place_cells(field, CELL_SHEEP_1, 1,  mirror_type=CELL_SHEEP_2)
+    place_cells(field, CELL_WOLF_1, 1, mirror_type=CELL_WOLF_2)
+    # place_cells(field, CELL_SHEEP_1, 1, point_mirror=True, mirror_type=CELL_SHEEP_2)
+    # place_cells(field, CELL_WOLF_1, 1, point_mirror=True, mirror_type=CELL_WOLF_2)
 
     store_map(name, field)
 
 
-generate_random_field("random_map", 0.5,
-                      {
-                          CELL_FENCE: 3,
-                          CELL_RHUBARB: 1,
-                          CELL_GRASS: 8
-                      })
+parser = argparse.ArgumentParser(description="Create Random Map")
+parser.add_argument("-n", "--name", help="name of random map")
+parser.add_argument("-e", "--entropy", help="name of random map")
+parser.add_argument("-ctc", "--cell_type_counts", help="name of random map")
+args = parser.parse_args()
+
+name = "random_map"
+
+
+if args.name:
+    name = args.name
+if args.entropy:
+    entropy = args.entropy
+else:
+    entropy = random.randint(20, 100) / 100
+if args.cell_type_counts:
+    cell_type_counts = args.cell_type_counts
+else:
+    cell_type_counts = {
+        CELL_FENCE: random.randint(0, 20),
+        CELL_RHUBARB: random.randint(0, 10),
+        CELL_GRASS: random.randint(1, 25)
+    }
+
+generate_random_field(name, entropy, cell_type_counts)
