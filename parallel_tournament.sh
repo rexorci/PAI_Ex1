eaba4d7a8cf29fb57f50822aa25b9b29c0d93bc5
Christoph Weber <christoph.t.weber@gmail.com>
2019-03-27 22:52:34 +0100

    script to let multiple matches run in parallel on random maps

---
+++ parallel_tournament.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "Start Virtual Environment"
+source venv/bin/activate
+
+n_maps=2
+
+ts=$(date "+%Y%m%d%H%M%S")
+log_name=$(echo logs/tournament_result_$ts.log)
+log_name_rev=$(echo logs/tournament_result_rev_$ts.log)
+
+touch log_name
+touch log_name_rev
+
+script1=chriweb_A1
+script2=greedy_player
+player1=IntrepidIbex
+player2=GreedyPlayer
+
+
+for i in $(seq 1 $n_maps)
+do
+    python map_generator.py -n "tournament_$i";
+done
+
+for i in $(seq 1 $n_maps)
+do
+    python kingsheep_tournament.py resources/random_maps/tournament_$i.map -p1m $script1 -p1n $player1 -p2m $script2 -p2n $player2 >> "$log_name" &
+    python kingsheep_tournament.py resources/random_maps/tournament_$i.map -p1m $script2 -p1n $player2 -p2m $script1 -p2n $player1 >> "$log_name_rev" &
+done
+wait
+
+
+deactivate
+
